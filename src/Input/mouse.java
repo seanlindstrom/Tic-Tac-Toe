0d100819f1042e56a3bad7d6222ae200212804bd
Julia Rakas <jrakas4@gmail.com>
2016-07-07 23:27:24 -0400

    .

--- src/Input/mouse.java
+++ src/Input/mouse.java
@@ -1,12 +1,27 @@
 package Input;
 import org.lwjgl.glfw.GLFWCursorPosCallback;
 
-public class mouse extends GLFWCursorPosCallback {
 
+public class mouse extends GLFWCursorPosCallback {
+private int X;
+private int Y;
 	@Override
 	public void invoke(long arg0, double arg1, double arg2) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/** Returns x coordinate of mouse on display */
+	public int xPos() {
+		//code code code (updates x)... x = ...
+		return X;
+	}
+	
+	/** Returns y coordinate of mouse on display */
+	public int yPos() {
+		//code code code (updates y)... y = ...
+		return Y;
+	}
 
 }
+
