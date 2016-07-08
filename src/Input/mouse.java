0e0d643dc58b6aab1e44b7e01a4539c6ece5edcd
Julia Rakas <jrakas4@gmail.com>
2016-07-07 23:19:21 -0400

    yaaayy

---
+++ src/Input/mouse.java
@@ -0,0 +1,12 @@
+package Input;
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+
+public class mouse extends GLFWCursorPosCallback {
+
+	@Override
+	public void invoke(long arg0, double arg1, double arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
